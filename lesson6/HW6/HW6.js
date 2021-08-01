e50808c0ee300a70bbbea5a011c43e436ec903ca
Nekit <d3m0nhunt3r92@gmailcom>
2021-08-01 16:09:15 +0300

    Homework 6 is done

--- lesson6/HW6/HW6.js
+++ lesson6/HW6/HW6.js
@@ -1,9 +1,41 @@
 // 1.Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then( value => {console.log(value)});
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then( value => {
+//         console.log(value);
+//         for (const item of value) {
+//             let count = 0;
+//             let div = document.createElement('div');
+//             let h2 = document.createElement('h2');
+//             let p = document.createElement('p');
+//
+//             h2.innerText = `${item.title}`;
+//             p.innerText = `${item.body}`;
+//
+//             div.append(h2,p);
+//             document.body.append(div);
+//             console.log(count++);
+//         }
+//     });
 
 // 2.Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
-fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(response => response.json())
-    .then(value => {console.log(value);});
+// fetch('https://jsonplaceholder.typicode.com/comments')
+//     .then(response => response.json())
+//     .then(value => {console.log(value);
+//         let div = document.createElement('div');
+//         for (const item of value) {
+//             let count = 0;
+//             let h2 = document.createElement('h2');
+//             let p = document.createElement('p');
+//
+//             h2.innerText = `${item.name}`;
+//             div.append(h2);
+//
+//             p.innerText = `${item.body}`;
+//             div.append(p);
+//             div.style.marginBottom = '15px';
+//
+//             document.body.append(div);
+//             console.log(count++);;
+//         }
+//     });
